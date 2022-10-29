8d2f3168fbe71a62fcce3cac679bf9e0b9ec5753
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-10-29 15:57:03 +0600

    main added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,7 +3,6 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
-import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 import RightSideNav from '../Pages/Shared/RightSideNav/RightSideNav';
 
 const Main = () => {
@@ -12,9 +11,6 @@ const Main = () => {
             <Header></Header>
             <Container>
                 <Row>
-                    {/* <Col lg="2" className='d-none d-lg-block'>
-                        <LeftSideNav></LeftSideNav>
-                    </Col> */}
                     <Col lg="10">
                         <Outlet></Outlet>
                     </Col>
