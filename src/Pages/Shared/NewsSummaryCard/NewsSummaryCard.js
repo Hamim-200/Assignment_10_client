57458fbd54ccb8e8e188924ccfa61d7870714671
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-10-29 22:01:25 +0600

    news summary added

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import Image from 'react-bootstrap/Image'
-import { FaEye, FaRegBookmark, FaShareAlt, FaStar } from 'react-icons/fa';
+import { FaEye } from 'react-icons/fa';
 import { Button } from 'react-bootstrap';
 
 const NewsSummaryCard = ({ news }) => {
-    const { _id, title, author, details, image_url, rating, total_view } = news;
+    const { title, details, image_url, total_view } = news;
 
     return (
-
-
-        // function BasicExample() {
-        //     return (
-        //         <Card style={{ width: '18rem' }}>
-        //             <Card.Img variant="top" src="holder.js/100px180" />
-        //             <Card.Body>
-        //                 <Card.Title>{title}</Card.Title>
-        //                 <Card.Text>
-        //                     {
-        //                         details.length > 250 ?
-        //                             <>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link> </>
-        //                             :
-        //                             details
-        //                     }
-        //                 </Card.Text>
-        //                 <Button variant="primary">Go somewhere</Button>
-        //             </Card.Body>
-        //         </Card>
-        //     );
-        // }
-
         <Card className="mb-5">
 
             < Card.Body >
@@ -38,17 +14,6 @@ const NewsSummaryCard = ({ news }) => {
                 <Card.Img variant="top" src={image_url} />
                 <Card.Text> {details} </Card.Text>
             </  Card.Body >
-
-            {/* <Card.Footer className="d-flex justify-content-between">
-                <div>
-                    <FaStar className='text-warning me-2'></FaStar>
-                    <span>{rating?.number}</span>
-                </div>
-                <div>
-                    <FaEye className='me-2'></FaEye>
-                    <span>{total_view}</span>
-                </div>
-            </Card.Footer> */}
             <Card.Footer className="d-flex justify-content-between" >
                 <div>
                     <Button variant="success">Enroll Now</Button>
