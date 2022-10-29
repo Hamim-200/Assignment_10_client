38d123f2f02e743238c1c59e98e4ad3d3a52dd64
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-10-29 21:30:29 +0600

    course added

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import News from '../News/News/News';
-// import { useLoaderData } from 'react-router-dom';
-// import News from '../News/News/News';
 import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
-// import NewsSummaryCard from '../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Course = () => {
     return (
@@ -16,15 +13,7 @@ const Course = () => {
                 </Col>
                 <Col lg="10">
                     <h2>All of our Courses </h2>
-                    {/* {
-                        allNews.map(news => <NewsSummaryCard
-                            key={news._id}
-                            news={news}
-                        ></NewsSummaryCard>)
-                    } */}
                     <News></News>
-
-
                 </Col>
 
             </Row>
